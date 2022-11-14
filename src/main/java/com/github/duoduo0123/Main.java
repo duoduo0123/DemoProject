61ec692026d63b667dcdf857aaf926e407a05118
duoduo <dvdii1tt@gmail.com>
2022-11-14 14:52:34 +0800

    Add another message

--- src/main/java/com/github/duoduo0123/Main.java
+++ src/main/java/com/github/duoduo0123/Main.java
@@ -2,6 +2,7 @@ package com.github.duoduo0123;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi World!");
+        System.out.println("另一行訊息");
     }
 }
