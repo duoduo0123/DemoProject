fa664bea78ac844695ae9e1edb78e8aa698d1b34
duoduo <dvdii1tt@gmail.com>
2022-11-14 14:50:50 +0800

    Update Hillo java

--- src/main/java/com/github/duoduo0123/Main.java
+++ src/main/java/com/github/duoduo0123/Main.java
@@ -2,6 +2,6 @@ package com.github.duoduo0123;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi world!");
+        System.out.println("Hillo Java!");
     }
 }
