b33d848ceee5d218f7ee37dccd7362c51c8ce6a7
duoduo <dvdii1tt@gmail.com>
2022-11-14 15:30:59 +0800

    Create ci.yml

---
+++ src/test/java/com/github/duoduo0123/CustomCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.github.duoduo0123;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomCalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+}
